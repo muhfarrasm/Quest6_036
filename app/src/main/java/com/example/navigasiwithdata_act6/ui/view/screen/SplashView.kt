8e3d5ae14ce2d6ca63242cf34a1efa2625a2c9d1
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 06:55:29 +0700

    menambahkan kolom

--- app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/SplashView.kt
@@ -1,2 +1,29 @@
 package com.example.navigasiwithdata_act6.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.navigasiwithdata_act6.R
+
+@Composable
+fun SplashView(
+    onMulaiButton: () -> Unit,
+){
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .background(
+            color = colorResource(
+                id = R.color.primary
+            )
+        ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
+}
