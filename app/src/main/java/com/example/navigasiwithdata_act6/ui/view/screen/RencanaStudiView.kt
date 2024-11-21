8e254730beee923769509874ebbbec593af8fe0c
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 06:43:31 +0700

    menambahkan kolom

--- app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/RencanaStudiView.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,9 +19,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.navigasiwithdata_act6.R
 import com.example.navigasiwithdata_act6.model.Mahasiswa
 import com.example.navigasiwithdata_act6.model.RencanaStudi
@@ -56,6 +61,25 @@ fun RencanaStudi(
                     .clip(shape = CircleShape)
                     .size(50.dp)
             )
+
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column (modifier = Modifier.weight(1f))
+            {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
+
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+
+            }
 
         }
 
