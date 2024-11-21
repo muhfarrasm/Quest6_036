f4838cf6a48938ef06dff0dcaed44268ab5074a3
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 05:58:04 +0700

    menambahkan kolom untuk meletakan text 1 dan 2

--- app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/MahasiswaFormView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,9 +16,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.navigasiwithdata_act6.R
 
 @Composable
@@ -63,7 +67,23 @@ fun MahasiswaFormView(
                 modifier = Modifier.size(45.dp)
             )
 
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Light
+                )
+            }
+
         }
+
 
     }
 
