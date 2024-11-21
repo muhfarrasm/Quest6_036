da6356c2893da07c6d9fb03cf5688dfe369da95e
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 05:39:48 +0700

    membuat kolom untuk dasaran layout

--- app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/MahasiswaFormView.kt
@@ -1,18 +1,24 @@
 package com.example.navigasiwithdata_act6.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.navigasiwithdata_act6.R
 
 @Composable
 fun MahasiswaFormView(
     modifier: Modifier,
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
-){
+) {
     var nama by remember {
         mutableStateOf("")
     }
@@ -27,3 +33,18 @@ fun MahasiswaFormView(
 
     val listData: MutableList<String> =
         mutableListOf(nim, nama, email)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+
+}
