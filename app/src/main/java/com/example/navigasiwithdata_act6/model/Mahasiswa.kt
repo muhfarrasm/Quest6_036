5e25319099dd58211ca5cfe4f6a2d3e194c8da22
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 05:18:21 +0700

    menambahkan data nim, nama, dan email di data class mahasiswa

--- app/src/main/java/com/example/navigasiwithdata_act6/model/Mahasiswa.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/model/Mahasiswa.kt
@@ -1,3 +1,7 @@
 package com.example.navigasiwithdata_act6.model
 
-data class Mahasiswa()
+data class Mahasiswa(
+    val nim: String = "",
+    val nama: String = "",
+    val email: String = ""
+)
