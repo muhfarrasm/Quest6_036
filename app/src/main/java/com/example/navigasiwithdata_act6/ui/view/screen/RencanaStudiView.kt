2cbd81fcad16f123a88ded4d25e8755c9a823d4c
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 06:33:11 +0700

    membuat kolom untuk layout dasar

--- app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/RencanaStudiView.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import com.example.navigasiwithdata_act6.R
 import com.example.navigasiwithdata_act6.model.Mahasiswa
 import com.example.navigasiwithdata_act6.model.RencanaStudi
 
@@ -27,6 +28,15 @@ fun RencanaStudi(
         mutableStateOf( "")
     }
     var listData: MutableList<String> = mutableListOf(chosenDropDown, pilihanKelas)
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    )
+    {
+
+    }
+
 
 
 }
