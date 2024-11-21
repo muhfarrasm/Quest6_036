a8f7ab8c1b0ef4609d84ac1313c72b2dcebabed3
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 05:30:50 +0700

    membuat variable email, nama, dan nim

--- app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/MahasiswaFormView.kt
@@ -1,2 +1,29 @@
 package com.example.navigasiwithdata_act6.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MahasiswaFormView(
+    modifier: Modifier,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+    var nama by remember {
+        mutableStateOf("")
+    }
+
+    var nim by remember {
+        mutableStateOf("")
+    }
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    val listData: MutableList<String> =
+        mutableListOf(nim, nama, email)
