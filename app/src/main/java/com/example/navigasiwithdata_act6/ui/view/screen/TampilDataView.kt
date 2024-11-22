54864f437fa08ab060fc6b8c48305bd2d606c6dd
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 17:16:14 +0700

    Membuat fungsi TampilDataView

---
+++ app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/TampilDataView.kt
@@ -0,0 +1,19 @@
+package com.example.navigasiwithdata_act6.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.navigasiwithdata_act6.model.Mahasiswa
+import com.example.navigasiwithdata_act6.model.RencanaStudi
+
+
+@Composable
+fun TampilDataView(
+    dataMhs: Mahasiswa,
+    dataKrs: RencanaStudi,
+    onBackButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+)
+{
+
+
+}
