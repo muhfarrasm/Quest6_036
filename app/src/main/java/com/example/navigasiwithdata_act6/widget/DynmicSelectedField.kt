3646581bd2ef9171c65cdbe9d8c93ee5e302aeb5
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 10:12:13 +0700

    membuat fungsi DynamicSelectedField

--- app/src/main/java/com/example/navigasiwithdata_act6/widget/DynmicSelectedField.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/widget/DynmicSelectedField.kt
@@ -1,2 +1,18 @@
 package com.example.navigasiwithdata_act6.widget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun DynamicSelectedField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangeEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
