a70671ac0492e726a2acbe043218859294c0783a
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 12:24:59 +0700

    Membuat checkbox dengan Row

--- app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/ui/view/screen/RencanaStudiView.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -146,6 +147,21 @@ fun RencanaStudi(
                             }
                         }
 
+                    }
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    HorizontalDivider()
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Text(text = "Klausul Persetujuan Mahasiswa", fontWeight = FontWeight.Bold)
+                    Row (verticalAlignment = Alignment.CenterVertically){
+                        Checkbox(
+                            checked = checked,
+                            onCheckedChange = {checked = it},
+                            enabled = chosenDropDown.isNotBlank() && pilihanKelas.isNotBlank()
+                        )
+                        Text(
+                            text = "Saya menyetujui setiap pernyataan yang ada tanpa paksaan dari pihak manapun.",
+                            fontWeight = FontWeight.Light, fontSize = 10.sp
+                        )
                     }
 
                 }
