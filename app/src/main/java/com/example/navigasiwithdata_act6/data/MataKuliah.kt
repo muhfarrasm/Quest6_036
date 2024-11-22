fca61e7af7e2af8ae6c11f1242ff685a6548b38c
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 10:22:58 +0700

    Menambahkan data di objek mata kuliah

--- app/src/main/java/com/example/navigasiwithdata_act6/data/MataKuliah.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.navigasiwithdata_act6.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
 }
