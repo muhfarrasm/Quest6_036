859a463bbcee5c74210dc65f52c277567c81a2bb
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 17:11:49 +0700

    Membuat class MahasiswaViewModel

---
+++ app/src/main/java/com/example/navigasiwithdata_act6/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.navigasiwithdata_act6.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.navigasiwithdata_act6.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+    private val _mahasiswaState = MutableStateFlow(Mahasiswa())
+    val mahasiswaStateUi : StateFlow<Mahasiswa> = _mahasiswaState.asStateFlow()
+
+    fun saveDataMahasiswa(ls: MutableList<String>) {
+        _mahasiswaState.update { status -> status.copy(
+            nim = ls[0],
+            nama = ls[1],
+            email = ls[2]) }
+    }
+}
