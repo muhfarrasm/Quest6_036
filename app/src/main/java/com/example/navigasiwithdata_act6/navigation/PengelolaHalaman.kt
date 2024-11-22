4a157be930180a18996e5361f1badbcad9b8875e
muhfarrasm <muhammadfarrasm@gmail.com>
2024-11-22 16:58:09 +0700

    Membuat fungsi PengelolaHalaman

--- app/src/main/java/com/example/navigasiwithdata_act6/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigasiwithdata_act6/navigation/PengelolaHalaman.kt
@@ -1,8 +1,24 @@
 package com.example.navigasiwithdata_act6.navigation
 
+import androidx.compose.runtime.Composable
+import com.example.navigasiwithdata_act6.ui.view.viewmodel.MahasiswaViewModel
+import com.example.navigasiwithdata_act6.ui.view.viewmodel.RencanaStudyViewModel
+import java.lang.reflect.Modifier
+
 enum class Halaman {
     Splash,
     Mahasiswa,
     Matakuliah,
     Tampil
+}
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+)
+{
+
 }
